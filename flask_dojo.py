9da07801454be713dbec6b4f7bf9f3b4b359ee25
aafonya <tolnai.judit12@gmail.com>
2017-02-22 09:45:36 +0100

    database and model definitions

---
+++ flask_dojo.py
@@ -0,0 +1,14 @@
+from peewee import *
+
+psql_db = PostgresqlDatabase('flask_dojo', user='postgres')
+
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+    class Meta:
+        database = psql_db
+
+
+class Counter(BaseModel):
+    get_request = IntegerField()
+    post_request = IntegerField()
